f736ab3c68bbea3edd3d35baa35aea3f25be4ea7
dnjsskarb7 <64752079+dnjsskarb7@users.noreply.github.com>
2020-07-10 15:56:23 +0900

    HOC added

--- client/src/reducers/authReducers.js
+++ client/src/reducers/authReducers.js
@@ -1,14 +1,12 @@
 import { AUTH_USER, CHANGE_AUTH, SIGN_OUT } from "../actions/types";
 const initial_state = {
   isLogin: null,
-  // changeAuth: true,
-  // signOut: false,
 };
 export default function (state = initial_state, action) {
   console.log(action);
   switch (action.type) {
     case AUTH_USER:
-      return {...state, isLogin: action.payload || false };
+      return { ...state, isLogin: action.payload || false };
     // case CHANGE_AUTH:
     //   return { ...state, changeAuth: action.payload };
     // case SIGN_OUT:
